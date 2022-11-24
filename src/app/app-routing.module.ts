2a70e92de56f3dee4a11a9e8abe64e93aa41adc5
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-11-24 09:01:35 +0700

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductCreateComponent} from "./product/product-create/product-create.component";
+import {ProductListComponent} from "./product/product-list/product-list.component";
+import {ProductEditComponent} from "./product/product-edit/product-edit.component";
+import {ProductDeleteComponent} from "./product/product-delete/product-delete.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'product/list',
+  component: ProductListComponent
+}, {
+  path: 'product/create',
+  component: ProductCreateComponent
+}, {
+  path: 'product/edit/:id',
+  component: ProductEditComponent
+}, {
+  path: 'product/delete/:id',
+  component: ProductDeleteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
